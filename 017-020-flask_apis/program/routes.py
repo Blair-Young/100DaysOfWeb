7a1e972ccbec5223b74c62fa90a8a8b2a32a9355
blair-young <byhillsboro@gmail.com>
2020-04-12 16:01:19 +0100

    Day 15- interatcion between routes and html through flask POST request

--- 017-020-flask_apis/program/routes.py
+++ 017-020-flask_apis/program/routes.py
@@ -1,5 +1,5 @@
 from program import app
-from flask import render_template
+from flask import render_template, request
 import datetime
 import requests
 
@@ -18,11 +18,30 @@ def chuck():
 	joke = get_chuck_quote()
 	return render_template('chuck.html', joke=joke)
 
+@app.route('/pokemon', methods=['GET', 'POST'])
+def pokemon():
+	pokemon = []
+	if request.method == 'POST' and 'pokecolour' in request.form:
+		colour = request.form.get('pokecolour')
+		print(colour)
+		pokemon = get_pokemon_based_on_color(colour)
+	return render_template('pokemon.html', pokemon=pokemon)
+
+def get_pokemon_based_on_color(colour):
+	endpoint = f'https://pokeapi.co/api/v2/pokemon-color/{colour.lower()}/'
+	r = requests.get(endpoint)
+	pokemon = []
+	for i in r.json()['pokemon_species']:
+		pokemon.append(i['name'])
+	return pokemon
 
 def get_chuck_quote():
 	r = requests.get('https://api.chucknorris.io/jokes/random')
 	data = r.json()
 	return data['value']
+
+
+
 
 
 
